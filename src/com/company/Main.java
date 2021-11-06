307f90538a1e76aa509af878843dcd9003e360bb
Kinara0 <kinarasoni@yahoo.com>
2021-11-05 21:59:25 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+StringBuilder sb = new StringBuilder("Kinara");
+        sb.append(" Chapaneri");
+System.out.println(sb);
+
+    }
+}
